6a8734e958780d08ae2bb79199168de86377ba6d
Love <love.lindqvist101@gmail.com>
2020-11-16 10:43:37 +0100

    Made Tuple

--- likeinhaskell/Tuple.java
+++ likeinhaskell/Tuple.java
@@ -3,7 +3,13 @@ package likeinhaskell;
 /**
  * Created by Niklas on 2016-02-07.
  */
-public class Tuple {
-    // public ... fst(){...}
-    // public ... snd(){...}
+public class Tuple<T,R> {
+    private final T first;
+    private final R second;
+    public Tuple(T t, R r){
+        first = t;
+        second = r;
+    }
+    public T fst(){return first;}
+    public R snd(){return second;}
 }
